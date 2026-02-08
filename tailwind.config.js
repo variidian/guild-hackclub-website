bf3fdad45b8ca8ed5073484c7ec41ed5c0da8239
turdle <shuruiguo1@gmail.com>
2026-02-08 15:43:00 +1100

    Add Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}", // adjust if your files are somewhere else
+    "./*.astro"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
