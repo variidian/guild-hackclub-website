f10b94bf131cb3369dac3af801094e0f74c16a8a
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2026-02-08 12:35:14 +0100

    Update to tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
 	integrations: [],
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
